20ddf355f53dfa6b8773ad24ff07f104cb324089
LeeInGyu <developLee20@gmail.com>
2021-07-27 22:36:18 +0900

    2021-07-27

--- Crowling/beautifulsoup4.py
+++ Crowling/beautifulsoup4.py
@@ -1,40 +1,12 @@
+from re import S
 import requests
 from bs4 import BeautifulSoup
-from openpyxl import Workbook
-
-def file_write(content):
-    f = open("test.txt",'w',encoding='UTF-8')
-
-    if type(content) == list:
-        for i in content:
-            f.write(i)
-    else:
-        f.write(content)
-        
-    f.close()
-
-# wb = Workbook()
-# ws = wb.active
-
-search_text = input("Search in Naver News: ")
-
-url = f'https://search.naver.com/search.naver?where=news&sm=tab_pge&query={search_text}&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=94&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=1'
-response = requests.get(url)
-
-assert response.status_code == 200
-
-newsParser = BeautifulSoup(response.content, "html.parser")
-title = newsParser.select('.news_tit')
-
-print(title[0])
-# file_write(title)
-
-# row = 2 * 10 - 10
-# for lst in data:
-#     title = lst.select('a.tit')[0].text
-#     date = lst.select('span.time')[0].text
-#     row = row + 1
-#     ws['A' + str(row)] = title
-#     ws['B' + str(row)] = date
-
-# wb.save('test.xlsx')
+url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%EB%84%A4%EC%9D%B4%EB%B2%84%EB%89%B4%EC%8A%A4"
+req = requests.get(url)
+print(req.text)
+#soup = BeautifulSoup(req.text)
+#print(req.text)
+#with open("temp.html","w") as f:
+#    f.write(req.text)
+#print(soup.find_all({"class":"list_news"}))
+##print(req.text.find('class="news_cluster"'))
--- Crowling/news.py
+++ Crowling/news.py
@@ -1,19 +1,67 @@
 import requests
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+import os
 
-def file_write(content):
-    f = open("test.txt",'w',encoding='UTF-8')
-    f.write(content)
-    f.close()
+path = './result'
 
-def url_open(content):
-    url = f'https://search.naver.com/search.naver?where=news&sm=tab_jum&query={content}'
-    crowling_content = requests.get(url)
+try:
+    os.mkdir(path)
+except FileExistsError:
+    pass
 
-    return crowling_content.text
+search_text = input("Enter the search content : ")
+page = int(input("How many page : "))
 
-# 완성 후 입력하게 변경
-search = '테니스'
-find_text = url_open(search)
+number = -9
 
-crowling_text = url_open(find_text)
-crowling_text.find('테니스')
+title_list = []
+link_list = []
+
+print('\n')
+print(f"{search_text}의 검색결과를 찾는 중입니다")
+print(f"총 {page}페이지 검색")
+
+for i in range(page):
+
+    print(f"검색 페이지 수 : {i+1} / {page}")
+
+    number += 10
+    url = f'https://search.naver.com/search.naver?where=news&sm=tab_pge&query={search_text}&start={number}'
+    content = requests.get(url)
+
+    assert content.status_code == 200
+
+    soup = BeautifulSoup(content.text, "html.parser")
+
+    minseo = soup.find("ul", {"class": "list_news"})
+    minseo = minseo.find_all("li")
+
+    for i in minseo:
+        try:
+            title = i.find("a", {"class":"news_tit"}).text
+            link = i.find("a", {"class":"news_tit"})
+            title_list.append(title)
+            link_list.append(link.get('href'))
+        except:
+            pass
+
+print("검색 완료!")
+print("파일에 저장중 . . .")
+
+wb = Workbook()
+ws = wb.active
+
+row = 2
+
+ws['A1'] = '뉴스명'
+ws['B1'] = '링크'
+
+for i, j in zip(title_list, link_list):
+    ws['A' + str(row)] = i
+    ws['B' + str(row)] = j
+    row += 1
+
+wb.save(f'{path}/{search_text}.xlsx')
+
+print("파일에 저장 완료!")
